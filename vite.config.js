84b01da08352a691b27d31fb89fd49f8acb78839
kiyanaa <hamzasezer1223@gmail.com>
2025-12-01 19:36:57 +0300

    vite config base path duzeltildi

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: './',   // <--- EN ÖNEMLİ KISIM BURASI (Virgülü unutma)
   plugins: [react()],
 })
